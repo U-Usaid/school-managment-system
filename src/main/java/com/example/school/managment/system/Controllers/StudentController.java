ba2ca8f858cc835c3b2d531176ffdca59d14579a
U-Usaid <osaid.osaid66@gmail.com>
2023-05-23 14:42:56 +0400

    Update StudentController

--- src/main/java/com/example/school/managment/system/Controllers/StudentController.java
+++ src/main/java/com/example/school/managment/system/Controllers/StudentController.java
@@ -1,4 +1,22 @@
 package com.example.school.managment.system.Controllers;
 
+import com.example.school.managment.system.Model.Student;
+import com.example.school.managment.system.Repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudent(){
+        return studentRepository.findAll();
+    }
 }

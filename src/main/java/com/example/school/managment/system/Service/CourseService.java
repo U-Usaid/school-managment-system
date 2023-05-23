4114fd5d868b69f96c3ceb3a0be8d6b1a37a8592
U-Usaid <osaid.osaid66@gmail.com>
2023-05-23 15:09:57 +0400

    Update CourseService

--- src/main/java/com/example/school/managment/system/Service/CourseService.java
+++ src/main/java/com/example/school/managment/system/Service/CourseService.java
@@ -1,7 +1,18 @@
 package com.example.school.managment.system.Service;
 
+import com.example.school.managment.system.Model.Course;
+import com.example.school.managment.system.Repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CourseService {
+    @Autowired
+    CourseRepository courseRepository;
+
+    public List<Course> getCourses(){
+        return courseRepository.findAll();
+    }
 }

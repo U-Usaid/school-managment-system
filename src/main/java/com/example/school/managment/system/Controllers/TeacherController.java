ee0d4ac947c626f13ce45b3e9862fb0485b65a24
U-Usaid <osaid.osaid66@gmail.com>
2023-05-23 14:54:17 +0400

    Update TeacherController

--- src/main/java/com/example/school/managment/system/Controllers/TeacherController.java
+++ src/main/java/com/example/school/managment/system/Controllers/TeacherController.java
@@ -1,4 +1,22 @@
 package com.example.school.managment.system.Controllers;
 
+import com.example.school.managment.system.Model.Teacher;
+import com.example.school.managment.system.Repositories.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teachers")
 public class TeacherController {
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @GetMapping
+    public List<Teacher> getAllTeachers(){
+        return teacherRepository.findAll();
+    }
 }

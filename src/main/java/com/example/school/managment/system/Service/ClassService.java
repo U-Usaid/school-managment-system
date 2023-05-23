3e2daaa0d669b9785702bf5c38d765d795a87c87
U-Usaid <osaid.osaid66@gmail.com>
2023-05-23 15:10:21 +0400

    Update ClassService

--- src/main/java/com/example/school/managment/system/Service/ClassService.java
+++ src/main/java/com/example/school/managment/system/Service/ClassService.java
@@ -1,8 +1,18 @@
 package com.example.school.managment.system.Service;
 
+import com.example.school.managment.system.Repositories.ClassRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClassService {
 
+    @Autowired
+    ClassRepository classRepository;
+
+    public List<Class> getClasses(){
+        return classRepository.findAll();
+    }
 }

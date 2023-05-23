2384939821513d59a9431f4220316a61485aee49
U-Usaid <osaid.osaid66@gmail.com>
2023-05-23 15:38:19 +0400

    Update postMapping in CourseController

--- src/main/java/com/example/school/managment/system/Controllers/CourseController.java
+++ src/main/java/com/example/school/managment/system/Controllers/CourseController.java
@@ -3,9 +3,7 @@ package com.example.school.managment.system.Controllers;
 import com.example.school.managment.system.Model.Course;
 import com.example.school.managment.system.Service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,10 @@ public class CourseController {
     @GetMapping
     public List<Course> getAllCourse(){
         return courseService.getCourses();
+    }
+
+    @PostMapping("/addCourse")
+    public Course createCourse(@RequestBody Course course){
+        return courseService.addCourse(course);
     }
 }

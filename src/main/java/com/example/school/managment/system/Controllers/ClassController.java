147ebab7c91cbf8bb7bfb031500f1c5e5c33c915
U-Usaid <osaid.osaid66@gmail.com>
2023-05-25 10:15:31 +0400

    adding put and delete

--- src/main/java/com/example/school/managment/system/Controllers/ClassController.java
+++ src/main/java/com/example/school/managment/system/Controllers/ClassController.java
@@ -1,6 +1,6 @@
 package com.example.school.managment.system.Controllers;
 
-import com.example.school.managment.system.Model.Class;
+import com.example.school.managment.system.Model.ClassRoom;
 import com.example.school.managment.system.Service.ClassService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,13 +14,21 @@ public class ClassController {
     ClassService classService;
 
     @GetMapping
-    public List<Class> getAllClasses(){
+    public List<ClassRoom> getAllClasses(){
         return classService.getClasses();
     }
 
     @PostMapping("/addClass")
-    public Class createClass(@RequestBody Class clas){
+    public ClassRoom createClass(@RequestBody ClassRoom clas){
         return classService.addClass(clas);
+    }
+
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteClass(@PathVariable int id) {
+         classService.deleteClass(id);
+        return "classRoom with" + id + "deleted";
+
     }
 
 

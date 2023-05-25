3b84278e4d596510acdb81455928dfedb261f399
U-Usaid <osaid.osaid66@gmail.com>
2023-05-25 14:41:24 +0400

    put and delete update in StudentService

--- src/main/java/com/example/school/managment/system/Service/StudentService.java
+++ src/main/java/com/example/school/managment/system/Service/StudentService.java
@@ -3,6 +3,7 @@ package com.example.school.managment.system.Service;
 import com.example.school.managment.system.Model.Student;
 import com.example.school.managment.system.Repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Indexed;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,5 +18,13 @@ public class StudentService {
     }
     public Student addStudent(Student student){
         return studentRepository.save(student);
+    }
+
+    public Student editStudent(Student student){
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Integer id){
+        studentRepository.deleteById(id);
     }
 }

e2d72c0a4132e3cc33259a5b3c37ca901d98dc9f
U-Usaid <osaid.osaid66@gmail.com>
2023-05-25 10:13:47 +0400

    update properties

---
+++ src/main/java/com/example/school/managment/system/Model/StudentCourse.java
@@ -0,0 +1,23 @@
+package com.example.school.managment.system.Model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Data
+@Entity
+public class StudentCourse {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+
+    int id;
+    float grade;
+
+}
